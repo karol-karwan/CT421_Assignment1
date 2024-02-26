285dc96c1fcf2b3c5f6371c6664d85a4bb2c0d86
karol-karwan <144912924+karol-karwan@users.noreply.github.com>
2024-02-26 15:47:57 +0000

    111

--- header20320721.h
+++ header20320721.h
@@ -2,11 +2,13 @@
 #ifndef AES_ENCRYPTION_H
 #define AES_ENCRYPTION_H
 
+#include <openssl/des.h>
 #include <openssl/aes.h>
 #include <openssl/rand.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
@@ -16,4 +18,9 @@ void encrypt_ecb(AES_KEY *aes_key, const unsigned char *data, unsigned char *enc
 void decrypt_ecb(AES_KEY *aes_key, const unsigned char *data, unsigned char *encrypted_data, size_t data_len);
 void decrypt_cbc(AES_KEY *aes_key, unsigned char *input, unsigned char *output, size_t length, unsigned char *iv);
 void encrypt_cbc(AES_KEY *aes_key, unsigned char *input, unsigned char *output, size_t length, unsigned char *iv);
+int decrypt_3des_ecb(unsigned char *ciphertext, int ciphertext_len, const_DES_cblock key1, const_DES_cblock key2, const_DES_cblock key3, unsigned char *plaintext);
+int encrypt_3des_ecb(unsigned char *plaintext, int plaintext_len, const_DES_cblock key1, const_DES_cblock key2, const_DES_cblock key3, unsigned char *ciphertext);
+int decrypt_3des_cbc(unsigned char *ciphertext, int ciphertext_len, const_DES_cblock key1, const_DES_cblock key2, const_DES_cblock key3, DES_cblock iv, unsigned char *plaintext);
+int encrypt_3des_cbc(unsigned char *plaintext, int plaintext_len, const_DES_cblock key1, const_DES_cblock key2, const_DES_cblock key3, DES_cblock iv, unsigned char *ciphertext);
+
 #endif // AES_ENCRYPTION_H
